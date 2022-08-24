8df6016c44a3e327e6ae5b44cd3bcadcad9ae866
emcofah <emfh21@student.bth.se>
2022-08-24 12:10:47 +0200

    Initial release

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { TrixEditor } from "react-trix";
+import "trix";
 
-function App() {
+
+export default function App() {
+  const [newHtml, setHTML] = useState("");
+  const [newText, setText] = useState("");
+
+  const handleChange = (html, text) => {
+    setHTML(html);
+    setText(text);
+  };
+  const handleSave = () => {
+    console.log(newText);
+  };
+  let mergeTags = [
+    {
+      trigger: "@",
+      tags: [
+        { name: "Dominic St-Pierre", tag: "@dominic" },
+        { name: "John Doe", tag: "@john" }
+      ]
+    },
+    {
+      trigger: "{",
+      tags: [
+        { name: "First name", tag: "{{ .FirstName }}" },
+        { name: "Last name", tag: "{{ .LastName }}" }
+      ]
+    }
+  ];
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>Text editor</h1>
+        <button type="button" className="trix-button trix-button--icon trix-button--icon-save" data-trix-attribute="save" title="Save" tabIndex="-1" onClick={handleSave}>Save</button>
+      </div>
+      <TrixEditor
+        className="custom-css-class"
+        autoFocus={true}
+        placeholder="Write something..."
+        value=""
+        uploadURL="https://domain.com/imgupload/receiving/post"
+        uploadData={{ "key1": "value", "key2": "value" }}
+        fileParamName="blob"
+        mergeTags={mergeTags}
+        onChange={handleChange}
+      />
+    </>
   );
 }
-
-export default App;
